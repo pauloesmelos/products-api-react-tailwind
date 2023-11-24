e7841b939fc4b8eb092450281bd90f267a7081ff
pauloesmelos <pauloempdel@hotmailcom>
2023-11-24 15:06:30 -0300

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  // Utilize a API abaixo para puxar a lista de produto
+  // https://ranekapi.origamid.dev/json/api/produto
+  // Cada produto possui o id, o mesmo pode ser passado na api para retornar os dados desse produto específico
+  // https://ranekapi.origamid.dev/json/api/produto/notebook
+
+  return (
+    <div className="font-Poppins">
+      <h1 className="text-rose-500">hello world</h1>
+    </div>
+  )
+}
+
+export default App;
