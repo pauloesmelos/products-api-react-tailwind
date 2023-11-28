231c0b7e4340521b5409c46524a0f0408d356f01
pauloesmelos <pauloempdel@hotmailcom>
2023-11-28 08:53:24 -0300

    added screen  specify product

--- src/components/product/SpecifyProduct.jsx
+++ src/components/product/SpecifyProduct.jsx
@@ -10,7 +10,22 @@ const SpecifyProduct = () => { // produto específico
   if(isLoading) return <Loading />
   return (
     <section className="">
-      <h1>{data.nome}</h1>
+      <div className="grid grid-cols-2 gap-5">
+        <div>
+          <h1>
+            <img src={data.fotos[0].src} alt={`${data.nome}`} title={`${data.nome}`} />
+          </h1>
+        </div>
+        <div className="flex flex-col">
+          <div>
+            <span className="text-[.7rem] sm:text-sm text-gray-600">Novo | + 1000 vendidos</span>
+          </div>
+          <div>
+            <h1 className="font-bold text-sm sm:text-xl">{data.descricao}</h1>
+            <p>R$ {data.preco}</p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
